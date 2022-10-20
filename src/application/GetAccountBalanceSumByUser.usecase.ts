8bf34e0b90b5c49be8d7fe34232d949eda928bbf
Marcos Silva <vinicius_marcosmartins@hotmail.com>
2022-10-20 20:28:40 -0300

    feat: finishing account balance sum endpoint and fixing some typos

--- src/application/GetAccountBalanceSumByUser.usecase.ts
+++ src/application/GetAccountBalanceSumByUser.usecase.ts
@@ -1,9 +1,8 @@
 import { User } from "../domain/entities/User";
 import { IAccountRepository } from "../domain/repositories/IAccountRepository";
-import { IUserRepository } from "../domain/repositories/IUserRepository";
 import { convertCurrency } from "../shared/utils/convertCurrency";
 
-class GetSumAccountBalanceByUser {
+class GetAccountBalanceSumByUser {
   private accountRepository: IAccountRepository;
 
   constructor(
@@ -15,8 +14,10 @@ class GetSumAccountBalanceByUser {
   execute(user: User): number {
     const accounts = this.accountRepository.findByUserId(user.id);
 
-    const sumBalances = accounts.reduce((acc, account) => acc += convertCurrency(account.currency, "USD", account.balance), 0);
+    const balancesSum = accounts.reduce((acc, account) => acc += convertCurrency(account.currency, "USD", account.balance), 0);
 
-    return sumBalances;
+    return balancesSum;
   }
 }
+
+export { GetAccountBalanceSumByUser };
